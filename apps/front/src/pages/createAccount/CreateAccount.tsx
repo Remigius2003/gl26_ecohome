6058114070ec89cb09a76ee07eac453dd7b78d3e
Antonin Djouder-Fey <antonin.djouder-fey@universite-paris-saclay.fr>
2025-12-13 12:23:27 +0100

    Create Account (pas pu tester car pb avec le startsWith)

--- apps/front/src/pages/createAccount/CreateAccount.tsx
+++ apps/front/src/pages/createAccount/CreateAccount.tsx
@@ -7,5 +7,56 @@ export default async function createAccount() {
   const t: RefreshToken = await login({ username: "blup", password: "blup" });
   console.log(t);
 
-  return <h1>createAccount</h1>;
+  return (
+        <div class="create_account-container">
+            <img 
+                src="login/uneMAISON.png"
+                alt="Illustration"
+                
+            />
+            <input 
+                type="text"
+                placeholder="Nom d'utilisateur"
+                style={{
+                    width: "100%",
+                    padding: "10px",
+                    border: "1px solid #ccc"
+                }}
+            />
+            <input 
+                type="email"
+                placeholder="Email"
+                style={{
+                    width: "100%",
+                    padding: "10px",
+                    border: "1px solid #ccc"
+                }}
+            />
+            <input 
+                type="password"
+                placeholder="Mot de passe"
+                style={{
+                    width: "100%",
+                    padding: "10px",
+                    border: "1px solid #ccc"
+                }}
+            />
+            <input 
+                type="password"
+                placeholder="Confirmer le mot de passe"
+                style={{
+                    width: "100%",
+                    padding: "10px",
+                    border: "1px solid #ccc"
+                }}
+            />
+
+            <button>
+                Créer un compte
+            </button>
+          
+          </div>
+    );
+          
+
 }
