a336179a3342b255c0a114139486d75ac9238512
AntoninDjouderFey <103748678+AntoninDjouderFey@users.noreply.github.com>
2025-12-14 20:35:26 +0100

    Front - Login/Register/Welcome pages (#1)
    * je suis une grosse merde ! pardon d'exister Rémi
    
    * J'en ai marre de vivre
    
    * Create Account (pas pu tester car pb avec le startsWith)
    
    * fallback on api path
    
    * réactoin pull request + création du welcome + pas encore tester, mon pc charge mal l'environnement pour le front
    
    ---------
    
    Co-authored-by: Zephiel 3 <zephiel.angel@gmail.com>

--- apps/front/src/App.tsx
+++ apps/front/src/App.tsx
@@ -11,30 +11,31 @@ const Register = lazy(() => import("@pages/auth/Register"));
 const Login = lazy(() => import("@pages/auth/Login"));
 
 const Layout = (props: any) => (
-  <>
-    <header>
+    <>
+        {/*<header>
       <h1>Eco Home</h1>
       <nav style={{ "margin-bottom": "1rem" }}>
         <A href="/">Home</A> |<A href="/dev">Dev Page</A> |{" "}
         <A href="/settings">Settings</A>
       </nav>
-    </header>
-    {props.children}
-  </>
+    </header>*/}
+        {props.children}
+    </>
 );
 
 export default function App() {
-  return (
-    <div style={{ "font-family": "sans-serif", padding: "1rem" }}>
-      <Router root={Layout}>
-        <Route path="/" component={Welcome} />
-        <Route path="/dev" component={DevPage} />
-        <Route path="/login" component={Login} />
-        <Route path="/social" component={Social} />
-        <Route path="/register" component={Register} />
-        <Route path="/settings" component={Settings} />
-        <Route path="*404" component={NotFound} />
-      </Router>
-    </div>
-  );
+    return (
+        <div style={{ "font-family": "sans-serif", padding: "1rem" }}>
+            <Router root={Layout}>
+                <Route path="/" component={DevPage} />
+                <Route path="/dev" component={DevPage} />
+                <Route path="/login" component={Login} />
+                <Route path="/social" component={Social} />
+                <Route path="/register" component={Register} />
+                <Route path="/welcome" component={Welcome} />
+                <Route path="/settings" component={Settings} />
+                <Route path="*404" component={NotFound} />
+            </Router>
+        </div>
+    );
 }
